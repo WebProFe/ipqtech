3a31551e796b1a724ae6c0d31cb1674eb073d7c2
WebProFe <medina.techie@gmail.com>
2019-02-06 15:04:34 -0600

    Nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import NavMenu from './components/Navigation/NavMenu';
+import NavMenu from './Components/Navigation/NavMenu';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Components/Pages/Home'
 
@@ -12,7 +12,7 @@ class App extends Component {
       <Router>
       <div>
         <NavMenu />
-        <Route exact path="/" component={Home} />
+        {/* <Route exact path="/" component={Home} /> */}
       </div>
     </Router>
     );
